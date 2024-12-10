b088eedfd8c213c98b0c56185bb135e6cb20b77e
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-12-10 14:06:37 +0100

    .

--- src/context/pharmacy-context.jsx
+++ src/context/pharmacy-context.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import { retrieveData } from '../utils/storage';
 
 const PharmacyContext = createContext(null);
